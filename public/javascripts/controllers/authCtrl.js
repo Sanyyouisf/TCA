0aea53e47a65e7017f862f607c333efb44c9644f
Sanyyouisf <sanymamdoh@yahoo.com>
2017-06-19 21:25:08 -0500

    tittle font

--- public/javascripts/controllers/authCtrl.js
+++ public/javascripts/controllers/authCtrl.js
@@ -8,15 +8,12 @@ app.controller("authCtrl", function($scope,AuthFactory,UserFactory,$location,$ro
 	let logMeIn = () => {
 		AuthFactory.authenticate($scope.auth)
 		.then((usercred) => {
-    		// console.log("usercred in logMeIn :",usercred);
     		return UserFactory.getUser(usercred.uid);
 		},(error)=>{
 			console.log("error in authenticate:",error);
 		})
 		.then((user)=>{
-    		console.log("user",user);
     		$rootScope.user = user ;
-      		console.log("$rootScope.user in logMeIn :",$rootScope.user);
     		$location.url('/parentChildLogin');
 		})
 		.catch((error)=>{
@@ -29,15 +26,12 @@ app.controller("authCtrl", function($scope,AuthFactory,UserFactory,$location,$ro
 	$scope.registerUser = () => {
 		AuthFactory.registerWithEmail($scope.auth)
 		.then((didRegister)=>{
-			// console.log("didRegister in didRegister: ", didRegister);
 			$scope.auth.uid = didRegister.uid;
 			return UserFactory.addUser($scope.auth);
-			// console.log("$scope.auth in registerUser: ", $scope.auth);
 		},(error)=>{
 			console.log("error in registerWithEmail:",error);
 		})
 		.then((registerComplete)=>{
-    		// console.log("registerComplete in registerUser:",registerComplete);
     		logMeIn();
 		})
 		.catch((error)=>{
