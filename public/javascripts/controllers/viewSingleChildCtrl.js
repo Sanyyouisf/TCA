c9311b1f76c49da316a7a73dde5c32f67e84473e
Sanyyouisf <sanymamdoh@yahoo.com>
2017-06-24 16:41:59 -0500

    display the name in single child activity

--- public/javascripts/controllers/viewSingleChildCtrl.js
+++ public/javascripts/controllers/viewSingleChildCtrl.js
@@ -7,45 +7,6 @@ app.controller("viewSingleChildCtrl", function($routeParams,$scope,AuthFactory,C
 	$scope.activityId = "";
 	$scope.childActivities =[];
 
-	// let displaySingleChildData = () =>{
-	//  	console.log("$routeParams.childId :",$routeParams.childId);
-	//  	ChildFactory.getSingleChild($routeParams.childId)
-	//     .then((result) => {
-	//         $scope.selectedChild = result;
-	//         console.log("$scope.selectedChild",$scope.selectedChild);
-	//         AvatarFactory.getSinglePicture($scope.selectedChild.pic)
-	//         .then((image) => {
-	//             $scope.selectedChild.url = image.path;
-	//         })
-	//         .catch((error) => {
-	//             console.log("error in getSinglePicture :", error);
-	//         });
-
-	// 	    ChildActivityFactory.getChildActivitiesForChild($scope.selectedChild.id)
-	// 	    .then((childActivities) => {
-	// 	    	// console.log("childActivities from getChildActivitiesForChild:",childActivities);
-	// 	        let kidActivities = [];
-	// 	        // console.log("childActivities", childActivities);
-	// 	        $scope.childActivities = childActivities;
-	// 	        // console.log("$scope.childActivities", $scope.childActivities);
-	// 	        childActivities.forEach((x) => {
-	// 	            // console.log("childActivities.isCompleted :",x.isCompleted);
-	// 	            ActivityFactory.getSingleActivity(x.activityId)
-	// 	            .then((result) => {
-	// 	            	result.childActivityId = x.id;
-	// 	                result.isCompleted = x.isCompleted;
-	// 	                kidActivities.push(result);
-	// 	            });
-	// 	        });
-	// 	        console.log("kidActivities",kidActivities);
-	// 	        $scope.activities = kidActivities;
-	// 	    });
-	//     })
-	//     .catch((error) => {
-	//         console.log("error in displayChildrenForParent: ", error);
-	//     });
-	// };
-
 	let displaySingleChildData = () =>{
 	 	console.log("$routeParams.childId :",$routeParams.childId);
 	 	ChildFactory.getSingleChild($routeParams.childId)
@@ -62,13 +23,9 @@ app.controller("viewSingleChildCtrl", function($routeParams,$scope,AuthFactory,C
 
 		    ChildActivityFactory.getChildActivitiesForChild($scope.selectedChild.id)
 		    .then((childActivities) => {
-		    	// console.log("childActivities from getChildActivitiesForChild:",childActivities);
 		        let kidActivities = [];
-		        // console.log("childActivities", childActivities);
 		        $scope.childActivities = childActivities;
-		        // console.log("$scope.childActivities", $scope.childActivities);
 		        childActivities.forEach((x) => {
-		            // console.log("childActivities.isCompleted :",x.isCompleted);
 		            ActivityFactory.getSingleActivity(x.activityId)
 		            .then((result) => {
 		            	result.childActivityId = x.id;
